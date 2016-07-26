a80772bc40cd32e81b8c8e18ff24ad84ecf06b3f
dbhat1 <dbhat@unomaha.edu>
2016-07-25 19:52:31 -0500

    adding users file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,13 +22,8 @@ class CreateUsersTable extends Migration
         });
 		
 			
-		$users = User::select('id', 'name', 'email', 'created_at')->get();
-		Excel::create('users', function($excel) use($users) {
-		$excel->sheet('Sheet 1', function($sheet) use($users) {
-        $sheet->fromArray($users);
-		});
-		})->export('xls');
-    }
+}
+
 
     /**
      * Reverse the migrations.
